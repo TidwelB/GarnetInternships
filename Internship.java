b924e3c2b29756c655b7ce4e3c2312a5aa6cd937
Nathan Russell <russellnathanielr@gmail.com>
2021-10-14 16:32:55 -0400

    adding Internship

---
+++ Internship.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author We Lit Design Company
+ */
+public class Internship {
+
+    //Create instance variables
+    private String position;
+    private ArrayList<String> requiredSkills;
+    private String recYear;
+    private double payrate;
+    private ArrayList<Student> applicants;
+    private int numOfApplicants;
+
+    public String getPosition() {
+
+    }
+
+    public ArrayList<String> getReqSkills() {
+
+    }
+
+    public String getRecYear() {
+
+    }
+
+    public double getPayrate() {
+
+    }
+
+    public ArrayList<Student> getApplicants() {
+        
+    }
+
+}
