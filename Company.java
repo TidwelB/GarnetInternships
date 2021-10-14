e847c14c38fa90968e5cd6a86aca77cf845bb447
Luke Howe <lahowe@email.sc.edu>
2021-10-14 17:50:50 -0400

    Adding professor.java

---
+++ Company.java
@@ -0,0 +1,34 @@
+/**
+ * @author We Lit Design Company
+ */
+
+ // Import packages
+ import java.util.ArrayList;
+
+public class Company extends Account {
+    
+
+    // Create instance variables
+    private Rating rating;
+    private ArrayList<Internship> availJobs;
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public ArrayList<Internship> getAvailJobs() {
+        return availJobs;
+    }
+
+    public void giveRating() {
+
+    }
+
+    public void addJob(Internship internship) {
+
+    }
+
+    public void removeJob(Internship internship) {
+
+    }
+}
---
+++ Professor.java
@@ -0,0 +1,22 @@
+/**
+ * @author We Lit Design Company
+ */
+
+public class Professor extends Account {
+    
+    // Create instance variables
+    private String email;
+    private String credentials;
+
+    public void giveRating() {
+
+    }
+
+    public String getEmail() {
+        return "";
+    }
+
+    public String getCredentials() {
+        return "";
+    }
+}
