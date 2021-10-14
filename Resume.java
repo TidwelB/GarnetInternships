bc227170a9d1554285a525752d7ec0dd37e28751
Luke Howe <lahowe@email.sc.edu>
2021-10-14 16:41:52 -0400

    Adding Education.java, Experience.java, and Resume.java

---
+++ Resume.java
@@ -0,0 +1,46 @@
+/**
+ * @author We Lit Design Company
+ */
+
+// Import packages
+import java.util.ArrayList;
+
+public class Resume {
+    // Create instance variables
+    private ArrayList<Education> priorEd;
+    private ArrayList<String> awards;
+    private ArrayList<Experience> relatedExp;
+    private ArrayList<Experience> commExp;
+    private ArrayList<String> skills;
+
+    // Create constructor
+    public Resume() {
+        priorEd = new ArrayList<Education>();
+        awards = new ArrayList<String>();
+        relatedExp = new ArrayList<Experience>();
+        commExp = new ArrayList<Experience>();
+        skills = new ArrayList<String>();
+    }
+
+    public ArrayList<Education> getPriorEd() {
+        return null;
+    }
+
+    public ArrayList<String> getAwards() {
+        return null;
+    }
+
+    public ArrayList<Experience> getRelatedExp() {
+        return null;
+    }
+
+    public ArrayList<Experience> getCommExp() {
+        return null;
+    }
+
+    public ArrayList<String> getSkills() {
+        return null;
+    }
+
+
+}
