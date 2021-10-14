528cf1029fcde742aa695b2ce1d9bf5f9bd8477b
Nathan Russell <russellnathanielr@gmail.com>
2021-10-14 16:47:07 -0400

    adding Account and Student

---
+++ Account.java
@@ -0,0 +1,22 @@
+/**
+ * @author We Lit Design Company
+ */
+public class Account {
+    
+    //Create instance variables
+    private String name;
+    private String username;
+    private String password;
+
+    public String getName() {
+        return "";
+    }
+
+    public String getUsername() {
+        return "";
+    }
+
+    public String setPassword(String password) {
+        return "";
+    }
+}
---
+++ Student.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+/**
+ * @author We Lit Design Company
+ */
+public class Student {
+
+    //Create instance variables
+    private String email;
+    private Resume resume;
+    private Rating rating;
+    private ArrayList<String> skills;
+    private String gradYear;
+    private ArrayList<Internship> listOfAppJobs;
+
+    public String getEmail() {
+
+    }
+
+    public Resume getResume() {
+
+    }
+
+    public int getRating() {
+
+    }
+
+    public ArrayList<String> getSkills() {
+
+    }
+
+    public String getGradYear() {
+
+    }
+
+    public void apply(Internship internship) {
+
+    }
+    
+    public ArrayList<Internship> getListOfAppJobs() {
+
+    }
+
+    public void giveRating(Rating Rating) {
+
+    }
+
+    public void editResume() {
+        
+    }
+}
