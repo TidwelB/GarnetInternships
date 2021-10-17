18dccb75ec814a98bf2e0cc307dd14afea2b37f3
Nathan Russell <russellnathanielr@gmail.com>
2021-10-17 17:56:47 -0400

    adding GarnetInternships and UI

---
+++ GarnetInternships.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+/**
+ * @author We Lit Design Company
+ */
+public class GarnetInternships {
+
+    //Create instance variables
+    private InternshipList internshipList;
+    private AccountList accountList;
+    private Account user;
+
+    public GarnetInternships() {
+
+    }
+
+    public ArrayList<Internship> viewInternships() {
+        return null;
+    }
+
+    public ArrayList<Internship> searchInternships(String keyword) {
+        return null;
+    }
+
+    public Account createAccount(String firstName, String lastName, String username) {
+        return user;
+    }
+
+    public Account login(String username, String password) {
+        return null;
+    }
+
+    public boolean logout() {
+        return false;
+    }
+}
---
+++ InternshipsUI.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * @author We Lit Design Company
+ */
+public class InternshipsUI {
+    private Scanner scanner;
+    private GarnetInternships application;
+
+    public void run() {
+
+    }
+
+    private void displayMainMenu() {
+        
+    }
+
+    public static void main(String[] args) {
+        InternshipsUI driver = new InternshipsUI();
+        driver.run();
+    }
+}
+
