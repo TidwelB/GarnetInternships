32aa927405b9ef7754ecec8b6042e0db0b5baf3f
Thomas Lloyd <talloyd@email.sc.edu>
2021-10-17 18:49:37 -0400

    working on json and dataconstants

--- DataConstants.java
+++ DataConstants.java
@@ -2,5 +2,18 @@
  * @author We Lit Design Company
  */
 public class DataConstants {
-    
+    protected static final String ACCOUNT_FILE_NAME = "/Account.json";
+    protected static final String ACCOUNT_NAME = "name";
+    protected static final String ACCOUNT_USER_NAME = "username";
+    protected static final String ACCOUNT_PASSWORD = "passord";
+    protected static final String ACCOUNT_PRIVILEGE = "privilege";
+    protected static final String ACCOUNT_ID = "id";
+    protected static final String PRIVILEGE_SPECIFIC = "privilegeSpecific";
+    protected static final String INTERNSHIP_FILE_NAME = "/Account.json";
+    protected static final String RESUME_FILE_NAME = "/Account.json";
+    protected static final String RESUME_ID = "id";
+    protected static final String RESUME_PRIOR_EDUCATION = "priorEd";
+    protected static final String RESUME_AWARDS = "awards";
+    protected static final String RESUME_RELATED_EXPERIENCE = "relatedExp";
+    protected static final String RESUME_COMMUNITY_EXPERIENCE = "commExp";
 }
