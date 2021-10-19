cabeeb22ce71d550466b0bd800f1d677e46cf77b
Luke Howe <lahowe@email.sc.edu>
2021-10-19 12:57:45 -0400

    Working on Edit Resume method in Student.java.

--- Student.java
+++ Student.java
@@ -1,8 +1,13 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  * @author We Lit Design Company
  */
+
+
 public class Student extends Account{
 
     //Create instance variables
@@ -49,6 +54,84 @@ public class Student extends Account{
     }
 
     public void editResume() {
+        // Create scanner for input
+        Scanner keyboard = new Scanner(System.in);
+        // Clear old resume
+        this.resume = null;
+
+        // ---------------------------------------------EDUCATION
+        System.out.println("Enter your school.");
+        try {
+            // Capture input
+            String school = keyboard.nextLine();
+        }
+        catch(InputMismatchException e) {
+            // Print error
+            System.out.println("Wrong input.");
+        }
+
+        System.out.println("Enter your graduation date.");
+        try {
+            String gradDate = keyboard.nextLine();
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Wrong input.");
+        }
+
+        System.out.println("Enter the location of your school.");
+        try {
+            String location = keyboard.nextLine();
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Wrong input.");
+        }
+        
+        System.out.println("Enter the name of your degree.");
+        try {
+            String degree = keyboard.nextLine();
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Wrong input.");
+        }
+
+        Education newEd = new Education(school, gradDate, location, degree);
+        
+    // ---------------------------------------------AWARDS
+
+        System.out.println("Enter your awards, comma delimited.");
+        try {
+            // Take input
+            String awardsUnsep = keyboard.nextLine();
+            // Split input by ,
+            String[] awardsSep = awardsUnsep.split(",");
+            // Create new ArrayList of String from split array
+            ArrayList<String> awardInput = new ArrayList<String>(Arrays.asList(awardsSep));
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Wrong input.");
+        }
+
+    // ---------------------------------------------EXPERIENCE
+
+        System.out.println("Enter an organization where you made an accomplishment.");
+        try {
+            //Take input
+            String expIn = keyboard.nextLine(); 
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Wrong input.");
+        }
+
+        System.out.println("Enter the location of that organization.");
+        try {
+            //Take input
+            String locationIn = keyboard.nextLine();
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Wrong input");
+        }
+
+
 
     }
 }
