3f5b660487dfe45850934cfcca51ebb118f280fb
tidwelb <tidwelb@email.sc.edu>
2021-10-21 08:46:12 -0400

    ResumeList class

--- DataWriter.java
+++ DataWriter.java
@@ -6,7 +6,7 @@
  import java.io.IOException;
  import java.util.ArrayList;
  import org.json.simple.JSONArray;
- import org.json.simpleJSONObject;
+ import org.json.simple.JSONObject;
 
 public class DataWriter extends DataConstants {
     
