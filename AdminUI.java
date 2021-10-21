5b998d389413aa936cfcdbc3a7104ddac508d7ae
Nathan Russell <russellnathanielr@gmail.com>
2021-10-21 09:22:41 -0400

    more UI stuff

--- AdminUI.java
+++ AdminUI.java
@@ -2,9 +2,11 @@
  * @author We Lit Design Company
  */
 public class AdminUI {
+
+    private static final String QUESTION = "What would you like to do?\n";
     
     public AdminUI() {
-        
+
     }
     
     public static String AdminMenu() {
@@ -12,7 +14,8 @@ public class AdminUI {
         adminMenu += "\nWelcome Administrator!";
         adminMenu += "--------Menu--------";
         adminMenu += "1. Hide Rating \n2. Remove Internship \n3. Delete Account\n";
-        adminMenu += "What would you like to do?\n";
+        adminMenu += QUESTION;
         return adminMenu;
     }
+
 }
