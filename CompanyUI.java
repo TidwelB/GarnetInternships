619f66cf0cc823f2dfb6d131290042a7b945a474
Nathan Russell <russellnathanielr@gmail.com>
2021-10-21 09:03:16 -0400

    adding all UI's, create account, and main UI clean up

---
+++ CompanyUI.java
@@ -0,0 +1,19 @@
+/**
+ * @author We Lit Design Company
+ */
+public class CompanyUI {
+
+    public CompanyUI() {
+        
+    }
+    
+    public static String CompanyMenu() {
+        String companyMenu = "";
+        companyMenu+= "\nWelcome 'Company'!";
+        companyMenu += "--------Menu--------";
+        companyMenu+= "1. Edit Profile \n2. Post Internship \n3. Give a Rating\n";
+        companyMenu += "What would you like to do?\n";
+        return companyMenu;
+    }
+    
+}
--- InternshipsUI.java
+++ InternshipsUI.java
@@ -15,6 +15,7 @@ public class InternshipsUI {
     }
 
     public void run() {
+        createAccount();
         System.out.println(WELCOME);
         login();
         while(true) {
@@ -39,28 +40,24 @@ public class InternshipsUI {
         String password = scanner.nextLine();
     }
 
+    private void createAccount() {
+        System.out.println("------------Create Account------------");
+        System.out.println("Enter Your University Network Username: ");
+        String username = scanner.nextLine();
+        System.out.println("Create Password: \n");
+        String password = scanner.nextLine();
+        System.out.println("Account Creation Success!");
+    }
+
     private void displayMainMenu() {
-        //NEEDS DATA FOR IF STUDENT, PROFESSOR, COMPANY, AND ADMINISTRATOR
         if(garnetInternships.getUser().getClass().getName().equals("Student")) {
-            System.out.println("\nWelcome 'Student'!");
-            System.out.println("--------Menu--------");
-            System.out.println("1. Edit Profile \n2. Edit Resume \n3. Search Internships\n");
-            System.out.println("What would you like to do?\n");
+            StudentUI.StudentMenu();
         } else if(garnetInternships.getUser().getClass().getName().equals("Professor")) {
-            System.out.println("\nWelcome Professor!");
-            System.out.println("--------Menu--------");
-            System.out.println("1. Edit Profile \n2. Give a Rating\n");
-            System.out.println("What would you like to do?\n");
+            ProfessorUI.ProfessorMenu();
         } else if(garnetInternships.getUser().getClass().getName().equals("Company")) {
-            System.out.println("\nWelcome 'Company'!");
-            System.out.println("--------Menu--------");
-            System.out.println("1. Edit Profile \n2. Post Internship \n3. Give a Rating\n");
-            System.out.println("What would you like to do?\n");
+            CompanyUI.CompanyMenu();
         } else if(garnetInternships.getUser().getClass().getName().equals("Admin")) {
-            System.out.println("\nWelcome Administrator!");
-            System.out.println("--------Menu--------");
-            System.out.println("1. Hide Rating \n2. Remove Internship \n3. Delete Account\n");
-            System.out.println("What would you like to do?\n");
+            AdminUI.AdminMenu();
         }
     }
 
