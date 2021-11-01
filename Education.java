edf7d7efd9c974976277a91b1c24db77682aee02
tidwelb <tidwelb@email.sc.edu>
2021-11-01 19:20:51 -0400

    Education JavaDoc

--- Education.java
+++ Education.java
@@ -4,13 +4,21 @@
 
 public class Education {
     
-    // Create instance variables
+    /**
+     * Instance variables strings of school, graduationDate, location, degree
+     */
     private String school;
     private String graduationDate;
     private String location;
     private String degree;
 
-    // Create constructor
+    /**
+     * Create constructor
+     * @param school school or univ where the student attended
+     * @param graduationDate date in which the student graduated or will graduate
+     * @param location where the school is located
+     * @param degree type of education recieved
+     */
     public Education(String school, String graduationDate, String location, String degree) {
         this.school = school;
         this.graduationDate = graduationDate;
@@ -18,22 +26,42 @@ public class Education {
         this.degree = degree;
     }
 
+    /**
+     * 
+     * @return returns the school in which a student has attended
+     */
     public String getSchool() {
         return this.school;
     }
 
+    /**
+     * 
+     * @return returns the date of graducation from the institution
+     */
     public String getGraduationDate() {
         return this.graduationDate;
     }
 
+    /**
+     * 
+     * @return returns the location of the school
+     */
     public String getLocation() {
         return this.location;
     }
 
+
+    /**
+     * 
+     * @return returns the type of education recieved
+     */
     public String getDegree() {
         return this.degree;
     }
 
+    /**
+     * Formats and prints information held within the education parameter (school ,grad date, location, and degree)
+     */
     public String toString() {
         String education = "----Education----";
         education += "\nSchool: "+school;
