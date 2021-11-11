2196ffc6c7a9f1ef942a5f50921304e2c64bb62f
Luke Howe <lahowe@email.sc.edu>
2021-11-11 14:04:45 -0500

    Added some tests in InternshipTest.java

--- InternshipTest.java
+++ InternshipTest.java
@@ -1,6 +1,7 @@
 // Import packages
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.UUID;
+import java.beans.Transient;
 import java.util.ArrayList;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -14,14 +15,22 @@ import org.junit.jupiter.api.Test;
 public class InternshipTest {
     
     @BeforeClass
-    public void oneTimeSetup() {
-        
+    public void setup() {
+	
     }
 
     @Test
-    public void testAddToApplicants() {
+    public void testAddToApplicantsNormal() {
         Internship testInternship = new Internship("Test Position", "Testing the program.", new ArrayList<String>(), "2025", 8.0, new ArrayList<Student>(), 0, UUID.randomUUID());
         Student testStudent = new Student("Test Account", "tester", "12345", "email@email.com", new Resume(), new Rating(), "2025", new ArrayList<Internship>(), UUID.randomUUID());
+        testInternship.addToApplicants(testStudent);
+        assertTrue(testInternship.getApplicants().contains(testStudent));
+    }
+
+    @Test
+    public void testAddToApplicantsNull() {
+        Internship testInternship = new Internship("Test Position", "Testing the program.", new ArrayList<String>(), "2025", 8.0, new ArrayList<Student>(), 0, UUID.randomUUID());
+        Student testStudent = new Student(null, null, null, null, null, null, null, null, null);
         testInternship.addToApplicants(testStudent);
         assertTrue(testInternship.getApplicants().contains(testStudent));
     }
@@ -38,6 +47,14 @@ public class InternshipTest {
     public void testGetCompany() {
         Internship testInternship = new Internship("Test Position", "Testing the program.", new ArrayList<String>(), "2025", 8.0, new ArrayList<Student>(), 0, UUID.randomUUID());
         Company testCompany = new Company("Test Company", "company", "12345", new Rating(), new ArrayList<Internship>(), UUID.randomUUID());
+        testCompany.addJob(testInternship);
+        assertEquals(testInternship.getCompany(),testCompany);
+    }
+
+    @Test
+    public void testGetCompanyNull() {
+        Internship testInternship = new Internship("Test Position", "Testing the program.", new ArrayList<String>(), "2025", 8.0, new ArrayList<Student>(), 0, UUID.randomUUID());
+        Company testCompany = new Company(null, null, null, null, null, null);
         testCompany.addJob(testInternship);
         assertEquals(testInternship.getCompany(),testCompany);
     }
