989ab859651d719d1d30038650ad89c6bfd044aa
Luke Howe <lahowe@email.sc.edu>
2021-11-11 17:57:20 -0500

    More progress

--- RatingTest.java
+++ RatingTest.java
@@ -1,4 +1,5 @@
 // Import packages
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -17,11 +18,8 @@ public class RatingTest {
     public void testAddRating() {
         
         Student testStudent = new Student("Test Account", "tester", "12345", "email@email.com", new Resume(), new Rating(), "2025", new ArrayList<Internship>(), UUID.randomUUID());
-        ArrayList<String> testArr = new ArrayList<String>();
-        testArr.add("Test rating");
-        Rating testRating = new Rating(5.0, testArr);
-        testStudent.getRating().addRating(5.0, "Test rating");
-        assertEquals(testStudent.getRating(), testRating);
+        testStudent.getRating().addRating(5.0, "test");
+        assertTrue(testStudent.getRating().getDescription().contains("test"));
     }
 
     @Test
