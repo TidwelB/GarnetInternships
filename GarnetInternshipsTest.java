2fa7761a4da786e25dcab1736c517f0f6123813c
Thomas Lloyd <talloyd@email.sc.edu>
2021-11-14 22:06:47 -0500

    more tests

--- GarnetInternshipsTest.java
+++ GarnetInternshipsTest.java
@@ -16,35 +16,42 @@ public class GarnetInternshipsTest {
 
     @Test
     public void testGetInstanceNull() {
-        assertFalse(GarnetInternships.getInstance() == null);
+        GarnetInternships garnetInternships = GarnetInternships.getInstance();
+        garnetInternships = null;
+        assertTrue(GarnetInternships.getInstance() != null);
     }
 
     @Test
     public void testLoginStudent() {
         Student testStudent = new Student("Test Account", "tester", "12345", "email@email.com", new Resume(), new Rating(), "2025", new ArrayList<Internship>(), UUID.randomUUID());
-        assertTrue(testStudent.getUsername().contains("tester"));
-        assertTrue(testStudent.getPassword().contains("12345"));
+        AccountList.getInstance().getAccounts().add(testStudent);
+        assertTrue(GarnetInternships.getInstance().login("tester", "12345"));
     }
 
     @Test
     public void testLoginProfessor() {
         Professor testProfessor = new Professor("Test Account", "tester", "12345", "email@email.com", "credentials", UUID.randomUUID());
-        assertTrue(testProfessor.getUsername().contains("tester"));
-        assertTrue(testProfessor.getPassword().contains("12345"));
+        AccountList.getInstance().getAccounts().add(testProfessor);
+        assertTrue(GarnetInternships.getInstance().login("tester", "12345"));
     }
 
     @Test
     public void testLoginCompany() {
         Company testCompany = new Company("Test Account", "tester", "12345", new Rating(), new ArrayList<Internship>(), UUID.randomUUID());
-        assertTrue(testCompany.getUsername().contains("tester"));
-        assertTrue(testCompany.getPassword().contains("12345"));
+        AccountList.getInstance().getAccounts().add(testCompany);
+        assertTrue(GarnetInternships.getInstance().login("tester", "12345"));
     }
 
     @Test
     public void testLoginAdmin() {
         Admin testAdmin = new Admin("Test Account", "tester", "12345", UUID.randomUUID());
-        assertTrue(testAdmin.getUsername().contains("tester"));
-        assertTrue(testAdmin.getPassword().contains("12345"));
+        AccountList.getInstance().getAccounts().add(testAdmin);
+        assertTrue(GarnetInternships.getInstance().login("tester", "12345"));
+    }
+
+    @Test
+    public void testLoginUserDoesNotExist() {
+        assertFalse(GarnetInternships.getInstance().login("not here", "password"));
     }
 
     @Test
@@ -61,11 +68,21 @@ public class GarnetInternshipsTest {
 
     @Test
     public void testLogout() {
+        AccountList.getInstance().getAccounts().clear();
+        Professor testProfessor = new Professor("Test Account", "tester", "12345", "email@email.com", "credentials",
+                UUID.randomUUID());
+        AccountList.getInstance().getAccounts().add(testProfessor);
+        GarnetInternships.getInstance().login("tester", "12345");
+        assertTrue(GarnetInternships.getInstance().logout());
+    }
+
+    @Test
+    public void testLogoutNoOneLoggedIn() {
         assertTrue(GarnetInternships.getInstance().logout() == false);
     }
 
     @Test
-    public void StayLoggedIn() {
+    public void testStayLoggedIn() {
         assertTrue(GarnetInternships.getInstance().logout() == false);
     }
 }
